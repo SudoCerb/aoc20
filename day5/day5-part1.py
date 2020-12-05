6cb422d461fa96e495ef80197c1b9455cbf745ff
SudoCerb <SudoCerb>
2020-12-05 09:27:35 +0200

    Compacted the code at the cost of readability

--- day5/day5-part1.py
+++ day5/day5-part1.py
@@ -3,10 +3,7 @@ file = open('input.txt','r')
 lines = file.read().splitlines()
 maxSeatId = 0
 for line in lines:
-    upperRow = 128 - 1
-    lowerRow = 0
-    upperCol = 8 - 1
-    lowerCol = 0
+    upperRow, lowerRow, upperCol, lowerCol = 128 - 1, 0, 8 - 1, 0
     for char in line:
         if char == 'F':
             upperRow = math.floor(upperRow - (upperRow - lowerRow) / 2)
@@ -16,8 +13,6 @@ for line in lines:
             upperCol = math.floor(upperCol - (upperCol - lowerCol) / 2)
         elif char == 'R':
             lowerCol = math.ceil(lowerCol + (upperCol - lowerCol) / 2)
-    seatId = upperRow * 8 + upperCol
-    if seatId > maxSeatId:
-        maxSeatId = seatId
-    print('LINE:', line, 'ROW:', upperRow, 'COL:', upperCol, 'SEAT_ID:', seatId)
+    if upperRow * 8 + upperCol > maxSeatId:
+        maxSeatId = upperRow * 8 + upperCol
 print('MAX_SEAT_ID: ',maxSeatId)
--- day5/day5-part2.py
+++ day5/day5-part2.py
@@ -1,13 +1,9 @@
 import math
 file = open('input.txt','r')
 lines = file.read().splitlines()
-maxSeatId = 0
-seatIds = []
+maxSeatId, seatIds = 0, []
 for line in lines:
-    upperRow = 128 - 1
-    lowerRow = 0
-    upperCol = 8 - 1
-    lowerCol = 0
+    upperRow, lowerRow, upperCol, lowerCol = 128 - 1, 0, 8 - 1, 0
     for char in line:
         if char == 'F':
             upperRow = math.floor(upperRow - (upperRow - lowerRow) / 2)
@@ -17,12 +13,11 @@ for line in lines:
             upperCol = math.floor(upperCol - (upperCol - lowerCol) / 2)
         elif char == 'R':
             lowerCol = math.ceil(lowerCol + (upperCol - lowerCol) / 2)
-    seatId = upperRow * 8 + upperCol
-    seatIds.append(seatId)
-    if seatId > maxSeatId:
-        maxSeatId = seatId
+    seatIds.append(upperRow * 8 + upperCol)
+    if seatIds[-1] > maxSeatId:
+        maxSeatId = seatIds[-1]
 seatIds.sort()
 for i in range(len(seatIds)):
-    nextId = i + 1
-    if seatIds[nextId] - seatIds[i] != 1:
-        print('YOUR_SEAT_ID:', seatIds[i] + 1)
+    if seatIds[i + 1] - seatIds[i] != 1:
+        print('YOUR SEAT:', seatIds[i] + 1)
+        break
